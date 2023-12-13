e679586a79851e4a3a64cfb8335a698cea116287
ManderO9 <hossem.chetoui@gmail.com>
2023-12-14 00:02:12 +0100

    Added github actions to publish the website

--- nntrain.web/Program.cs
+++ nntrain.web/Program.cs
@@ -9,17 +9,3 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-
-
-
-//protected override async Task OnInitializedAsync()
-//{
-//    forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
-//}
-
-
-// Load the network from github using an http request
-// make sure the values are in correct order
-// normalize the values that you will read from the user
-// pass the values through the network
-// display the result
